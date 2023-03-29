cebfac546ef1de69b92e0cd7b58e28b54536d358
Morais <gabriel.de-souza-morais@epitech.eu>
2023-03-29 17:24:27 +0200

    [fix] folder name

--- lib/games/pacman/pacman_tools.cpp
+++ lib/games/pacman/pacman_tools.cpp
@@ -44,66 +44,66 @@ extern "C" std::string getType() {
 
 std::vector<std::string> Pacman::getTextures() {
     std::vector<std::string> files_path = {
-        "lib/games/Pacman/files/wall.png",
+        "lib/games/pacman/files/wall.png",
         // lib/games/pacman/files/player-down-1.png
-        "lib/games/Pacman/files/player-right-1.png",
-        "lib/games/Pacman/files/player-right-2.png",
-        "lib/games/Pacman/files/player-left-1.png",
-        "lib/games/Pacman/files/player-left-2.png",
-        "lib/games/Pacman/files/player-up-1.png",
-        "lib/games/Pacman/files/player-up-2.png",
-        "lib/games/Pacman/files/player-down-1.png",
-        "lib/games/Pacman/files/player-down-2.png",
+        "lib/games/pacman/files/player-right-1.png",
+        "lib/games/pacman/files/player-right-2.png",
+        "lib/games/pacman/files/player-left-1.png",
+        "lib/games/pacman/files/player-left-2.png",
+        "lib/games/pacman/files/player-up-1.png",
+        "lib/games/pacman/files/player-up-2.png",
+        "lib/games/pacman/files/player-down-1.png",
+        "lib/games/pacman/files/player-down-2.png",
         // lib/games/pacman/files/ghost-blue-1.png
-        "lib/games/Pacman/files/ghost-blue-1.png", // right
-        "lib/games/Pacman/files/ghost-blue-2.png",
-        "lib/games/Pacman/files/ghost-blue-3.png", // left
-        "lib/games/Pacman/files/ghost-blue-4.png",
-        "lib/games/Pacman/files/ghost-blue-5.png", // up
-        "lib/games/Pacman/files/ghost-blue-6.png",
-        "lib/games/Pacman/files/ghost-blue-7.png", // down
-        "lib/games/Pacman/files/ghost-blue-8.png",
+        "lib/games/pacman/files/ghost-blue-1.png", // right
+        "lib/games/pacman/files/ghost-blue-2.png",
+        "lib/games/pacman/files/ghost-blue-3.png", // left
+        "lib/games/pacman/files/ghost-blue-4.png",
+        "lib/games/pacman/files/ghost-blue-5.png", // up
+        "lib/games/pacman/files/ghost-blue-6.png",
+        "lib/games/pacman/files/ghost-blue-7.png", // down
+        "lib/games/pacman/files/ghost-blue-8.png",
         // lib/games/pacman/files/ghost-pink-1.png
-        "lib/games/Pacman/files/ghost-pink-1.png", // right
-        "lib/games/Pacman/files/ghost-pink-2.png",
-        "lib/games/Pacman/files/ghost-pink-3.png", // left
-        "lib/games/Pacman/files/ghost-pink-4.png",
-        "lib/games/Pacman/files/ghost-pink-5.png", // up
-        "lib/games/Pacman/files/ghost-pink-6.png",
-        "lib/games/Pacman/files/ghost-pink-7.png", // down
-        "lib/games/Pacman/files/ghost-pink-8.png",
+        "lib/games/pacman/files/ghost-pink-1.png", // right
+        "lib/games/pacman/files/ghost-pink-2.png",
+        "lib/games/pacman/files/ghost-pink-3.png", // left
+        "lib/games/pacman/files/ghost-pink-4.png",
+        "lib/games/pacman/files/ghost-pink-5.png", // up
+        "lib/games/pacman/files/ghost-pink-6.png",
+        "lib/games/pacman/files/ghost-pink-7.png", // down
+        "lib/games/pacman/files/ghost-pink-8.png",
         // lib/games/pacman/files/ghost-orange-1.png
-        "lib/games/Pacman/files/ghost-orange-1.png", // right
-        "lib/games/Pacman/files/ghost-orange-2.png",
-        "lib/games/Pacman/files/ghost-orange-3.png", // left
-        "lib/games/Pacman/files/ghost-orange-4.png",
-        "lib/games/Pacman/files/ghost-orange-5.png", // up
-        "lib/games/Pacman/files/ghost-orange-6.png",
-        "lib/games/Pacman/files/ghost-orange-7.png", // down
-        "lib/games/Pacman/files/ghost-orange-8.png",
+        "lib/games/pacman/files/ghost-orange-1.png", // right
+        "lib/games/pacman/files/ghost-orange-2.png",
+        "lib/games/pacman/files/ghost-orange-3.png", // left
+        "lib/games/pacman/files/ghost-orange-4.png",
+        "lib/games/pacman/files/ghost-orange-5.png", // up
+        "lib/games/pacman/files/ghost-orange-6.png",
+        "lib/games/pacman/files/ghost-orange-7.png", // down
+        "lib/games/pacman/files/ghost-orange-8.png",
         // lib/games/pacman/files/ghost-red-1.png
-        "lib/games/Pacman/files/ghost-red-1.png", // right
-        "lib/games/Pacman/files/ghost-red-2.png",
-        "lib/games/Pacman/files/ghost-red-3.png", // left
-        "lib/games/Pacman/files/ghost-red-4.png",
-        "lib/games/Pacman/files/ghost-red-5.png", // up
-        "lib/games/Pacman/files/ghost-red-6.png",
-        "lib/games/Pacman/files/ghost-red-7.png", // down
-        "lib/games/Pacman/files/ghost-red-8.png",
+        "lib/games/pacman/files/ghost-red-1.png", // right
+        "lib/games/pacman/files/ghost-red-2.png",
+        "lib/games/pacman/files/ghost-red-3.png", // left
+        "lib/games/pacman/files/ghost-red-4.png",
+        "lib/games/pacman/files/ghost-red-5.png", // up
+        "lib/games/pacman/files/ghost-red-6.png",
+        "lib/games/pacman/files/ghost-red-7.png", // down
+        "lib/games/pacman/files/ghost-red-8.png",
         // food
-        "lib/games/Pacman/files/small-food.png",
-        "lib/games/Pacman/files/medium-food.png",
-        "lib/games/Pacman/files/big-food.png",
+        "lib/games/pacman/files/small-food.png",
+        "lib/games/pacman/files/medium-food.png",
+        "lib/games/pacman/files/big-food.png",
         // ghost-weak-9 = 44
-        "lib/games/Pacman/files/ghost-weak-9.png",
-        "lib/games/Pacman/files/ghost-weak-10.png",
-        "lib/games/Pacman/files/ghost-weak-11.png",
-        "lib/games/Pacman/files/ghost-weak-12.png",
+        "lib/games/pacman/files/ghost-weak-9.png",
+        "lib/games/pacman/files/ghost-weak-10.png",
+        "lib/games/pacman/files/ghost-weak-11.png",
+        "lib/games/pacman/files/ghost-weak-12.png",
         // ghost-eyes-9 = 48
-        "lib/games/Pacman/files/ghost-eyes-9.png",
-        "lib/games/Pacman/files/ghost-eyes-10.png",
-        "lib/games/Pacman/files/ghost-eyes-11.png",
-        "lib/games/Pacman/files/ghost-eyes-12.png"
+        "lib/games/pacman/files/ghost-eyes-9.png",
+        "lib/games/pacman/files/ghost-eyes-10.png",
+        "lib/games/pacman/files/ghost-eyes-11.png",
+        "lib/games/pacman/files/ghost-eyes-12.png"
     };
     return files_path;
 }
